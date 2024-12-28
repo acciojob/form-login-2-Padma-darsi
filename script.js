d2e711d9b6ab537f0d8d01b6ed66ebd0def44027
Padma-darsi <125295009+Padma-darsi@users.noreply.github.com>
2024-12-28 19:48:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+let fname=document.getElementById("f-name").value;
+let lname=document.getElementById("l-name").value;
+let num=document.getElementById("num").value;
+let mail=document.getElementById("mail").value;
+
+function info
+	{
+	   
+	}
