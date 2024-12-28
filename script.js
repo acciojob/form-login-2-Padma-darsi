2af3886b9364c2fb29fe3fce3f6de3f982255fb1
Padma-darsi <125295009+Padma-darsi@users.noreply.github.com>
2024-12-28 19:59:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,9 +3,9 @@
 function info
 	{
 	   const fname=document.getElementById("f-name").value;
-const lname=document.getElementById("l-name").value;
-const num=document.getElementById("num").value;
-const mail=document.getElementById("mail").value;
+       const lname=document.getElementById("l-name").value;
+       const num=document.getElementById("num").value;
+       const mail=document.getElementById("mail").value;
 	   const alertMessage=`First Name:${fname} Last Name: ${lname} Phone Number: ${num} Email ID: ${mail}`;
 	alert(alertMessage);
 	}
