33b1311c1482e826a3a07ca52252465e4a011e5e
Padma-darsi <125295009+Padma-darsi@users.noreply.github.com>
2024-12-28 20:12:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
+function info(event) {
+    // Prevent form submission
+    event.preventDefault();
 
+    // Get form values
+    const fname = document.getElementById("f-name").value;
+    const lname = document.getElementById("l-name").value;
+    const num = document.getElementById("num").value;
+    const mail = document.getElementById("mail").value;
 
-function info(event)
-	{
-		 event.preventDefault();
-	   const fname=document.getElementById("f-name").value;
-       const lname=document.getElementById("l-name").value;
-       const num=document.getElementById("num").value;
-       const mail=document.getElementById("mail").value;
-	   const alertMessage=`First Name:${fname} Last Name: ${lname} Phone Number: ${num} Email ID: ${mail}`;
-	alert(alertMessage);
-	}
+    // Create and display the alert message
+    const alertMessage = `First Name: ${fname} Last Name: ${lname} Phone Number: ${num} Email ID: ${mail}`;
+    alert(alertMessage);
+}
