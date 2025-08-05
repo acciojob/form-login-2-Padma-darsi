322cd6c3ccce58b026a4bdc591871c415caf0ee6
Padma-darsi <125295009+Padma-darsi@users.noreply.github.com>
2025-08-05 06:59:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+const first=document.getElementById("first").value;
+const last=document.getElementById("last").value;
+const number=document.getElementById("num").value;
+const email=document.getElementById("email".value);
+
+const btn=document.getElementById("data").addEventListener("click",()=>
+	{
+		alert(`
+		       First Name: ${first} 
+			   Last Name: ${last}
+			   Phone Number: ${number}
+			   Email Id: ${email}`);
+		
+	});
